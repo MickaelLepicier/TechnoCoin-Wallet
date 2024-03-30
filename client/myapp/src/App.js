cf39834282cf30a43887653eb7cf950ef5e4337e
mickael lepicier <mickael233@gmail.com>
2024-03-30 21:29:34 +0300

    organizing the code wip

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -3,14 +3,11 @@ import { Home } from "./pages/home/Home.js";
 import { PageTwo } from "./pages/pageTwo/PageTwo.js";
 import { MenuBar } from "./components/menuBar/MenuBar.js";
 import { Footer } from "./components/footer/Footer.js";
-import "./App.css";
 import { ActionPage } from "./pages/actionPage/ActionPage.js";
 import { AnotherActionPage } from "./pages/anotherActionPage/AnotherActionPage.js";
-// import { useState } from "react";
+import "./App.css";
 
 function App() {
-  // const [darkModeOn, setDarkModeOn] = useState(false);
-
   return (
     <div className="app-container">
       <MenuBar />
